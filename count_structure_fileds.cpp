03a8c909d96bb60d2d50bc828ca3c26531041c8c
Violetta Fedotova <violettfaid@google.com>
2021-01-25 23:46:01 +0300

    init

---
+++ count_structure_fileds.cpp
@@ -0,0 +1,41 @@
+#include <utility>
+#include <string>
+#include <iostream>
+
+namespace detail {
+    template<std::size_t>
+    struct Caster {
+        template<typename U>
+        operator U() const noexcept;
+    };
+
+    template<typename T, std::size_t... I>
+    constexpr auto struct_size_impl(std::index_sequence<I...> args)
+    {
+        return args.size() - 1;
+    }
+
+    template<typename T, std::size_t Head, std::size_t... I>
+    constexpr auto struct_size_impl(std::index_sequence<Head, I...> args) -> decltype(T{Caster<Head>{}, Caster<I>{}...}, std::size_t{})
+    {
+        return struct_size_impl<T>(std::make_index_sequence<args.size() + 1>());
+    }
+}
+
+template<typename St>
+constexpr size_t struct_size() {
+    return detail::struct_size_impl<St>(std::make_index_sequence<1>{});
+}
+
+struct Foo {
+    int a;
+    double b;
+    char c;
+    const char* w;
+    std::string ss;
+};
+
+int main() {
+    constexpr size_t n = struct_size<std::string>();
+    std::cout << "n: " << n << std::endl;
+}
